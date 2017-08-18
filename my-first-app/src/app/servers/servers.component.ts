f49e84d092715bb2b68700c86f5f33ed22955b76
Franco Arratia <franco.arratia@dharbor.com>
2017-08-17 20:33:11 -0400

    Learning about property binding.

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-
 /*selector: 'app-server', --> with this notation in the html, this has to be used like: <app-servers></app-servers> -
                               This is the most common syntax*/
 /*selector: '[app-servers]', --> with this notation in the html, this has to be used like: <div app-servers></div>*/
@@ -13,7 +12,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewServer = false;
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 5000);
+  }
 
   ngOnInit() {
   }
