31cbb5cd0e7b0badd9e313ecb5da2efd0e07da2a
Franco Arratia <franco.arratia@dharbor.com>
2017-08-17 20:16:52 -0400

    Learning about String binding.

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -7,4 +7,10 @@ import {Component} from '@angular/core';
 })
 export class ServerComponent {
 
+  serverName = 'WinOSX';
+  isStarted = true;
+
+  isServerStarted() {
+    return this.isStarted;
+  }
 }
