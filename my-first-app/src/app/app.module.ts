410d6a12f2346b9abcd06b51f4fb778b5ae7d981
Franco Arratia <franco.arratia@dharbor.com>
2017-08-17 20:08:14 -0400

    Adding components and learning about selectors.

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -6,12 +6,14 @@ import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
 import {ServerComponent} from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+
+/*selector: 'app-server', --> with this notation in the html, this has to be used like: <app-servers></app-servers> -
+                              This is the most common syntax*/
+/*selector: '[app-servers]', --> with this notation in the html, this has to be used like: <div app-servers></div>*/
+/*selector: '.app-servers', --> with this notation in the html, this has to be used like: <div class="app-servers"></div>*/
+
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
