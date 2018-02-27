c2f0d06910fdaa688291927c6e6297e243be2701
franco-extreme <franck_ral@hotmail.com>
2018-02-27 15:07:59 -0400

    ng5-spa-components: Learning about RoutingLink and RouterLinkActive in Anguar

--- ng5-spa-heroes/src/app/app.routes.ts
+++ ng5-spa-heroes/src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
 
 const APP_ROUTES: Routes = [
   // With the following configuration the path is going to be: localhost:4200/home
   // If we do not need to specify /home, we will need to use the following.
-  //{ path: 'home', component: HomeComponent },
-  { path: '', component: HomeComponent, outlet: 'home' },
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  //{ path: '', component: HomeComponent, outlet: 'home' },
+  { path: 'about', component: AboutComponent },
+  { path: 'heroes', component: HeroesComponent },
   { path: '**', pathMatch: 'full', redirectTo: '' }
 ];
 
