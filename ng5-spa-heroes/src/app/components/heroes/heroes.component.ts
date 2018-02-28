439e7120a51dd4c53b31725099deefc890884ec2
franco-extreme <franck_ral@hotmail.com>
2018-02-28 11:57:37 -0400

    ng5-spa-components: Routes with parameters, implemented by following routerLink and Angular Route.

--- ng5-spa-heroes/src/app/components/heroes/heroes.component.ts
+++ ng5-spa-heroes/src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService, Heroe } from '../../services/heroes.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-heroes',
@@ -10,13 +11,18 @@ export class HeroesComponent implements OnInit {
 
   heroes:Heroe[] = [];
 
-  constructor( private _heroesService:HeroesService ) {
-    console.log("Constructor......");
+  constructor( private _heroesService:HeroesService, private router:Router ) {
+    //console.log("Constructor......");
   }
 
   ngOnInit() {
-    console.log("ng-on-init......");
+    //console.log("ng-on-init......");
 
     this.heroes = this._heroesService.getHeroes();
+  }
+
+  seeHero( idx:number ) {
+    //console.log(idx);
+    this.router.navigate(['/heroe', idx]);
   }
 }
