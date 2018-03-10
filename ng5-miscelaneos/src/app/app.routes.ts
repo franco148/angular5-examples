7546f4cacc2d61b0c52b28d740da0e05d9c14913
franco-extreme <franck_ral@hotmail.com>
2018-03-09 23:06:55 -0400

    ngMiscelaneos: Separating CHILD ROUTES in a specific file.

--- ng5-miscelaneos/src/app/app.routes.ts
+++ ng5-miscelaneos/src/app/app.routes.ts
@@ -3,21 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { UserComponent } from './components/user/user.component';
 
-import { NewUserComponent } from './components/user/new-user.component';
-import { EditUserComponent } from './components/user/edit-user.component';
-import { UserDetailComponent } from './components/user/user-detail.component';
+import { USER_ROUTES } from './components/user/user.routes';
 
 const APP_ROUTES: Routes = [
   { path: 'home', component: HomeComponent },
   {
     path: 'user/:id',
     component: UserComponent,
-    children: [
-      { path: 'new', component: NewUserComponent },
-      { path: 'edit', component: EditUserComponent },
-      { path: 'detail', component: UserDetailComponent },
-      { path: '**', pathMatch: 'full', redirectTo: 'new' }
-    ]
+    children: USER_ROUTES
   },
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
