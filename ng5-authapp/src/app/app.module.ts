ee36b8e9698fb9a2b631c0a6bf3d6cb7d8315c92
franco-extreme <franck_ral@hotmail.com>
2018-03-12 23:27:21 -0400

    ngAuthApp: Creating the UI for our application.

--- ng5-authapp/src/app/app.module.ts
+++ ng5-authapp/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//APP_ROUTES
+import { APP_ROUTING } from './app.routes';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { PricesComponent } from './components/prices/prices.component';
+import { ProtectedComponent } from './components/protected/protected.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    PricesComponent,
+    ProtectedComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
