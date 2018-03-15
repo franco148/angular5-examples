f14f7179314b863551334b3f701f1ce084d7d1a8
franco-extreme <franck_ral@hotmail.com>
2018-03-14 21:25:38 -0400

    ngAngularForms: Data - preparing the project to work with DATA aproximation approach in building forms with angular.

--- ng5-forms-examples/src/app/app.module.ts
+++ ng5-forms-examples/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 
@@ -18,6 +18,7 @@ import { DataComponent } from './components/data/data.component';
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule
   ],
   providers: [],
--- ng5-forms-examples/src/app/components/data/data.component.ts
+++ ng5-forms-examples/src/app/components/data/data.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
+//For using data aproximation feature we need to import ReactiveFormsModule in ap.module.ts
 @Component({
   selector: 'app-data',
   templateUrl: './data.component.html',
   styles: []
 })
-export class DataComponent implements OnInit {
+export class DataComponent {
 
-  constructor() { }
+  dataForm:FormGroup;
 
-  ngOnInit() {
+  constructor() {
+    this.dataForm = new FormGroup({
+      'name': new FormControl('Franco'), //second parameter for validation rules, third async validation
+      'lastName': new FormControl('Arratia'),
+      'email': new FormControl()
+    });
+  }
+
+  saveChanges() {
+    console.log(this.dataForm.value);
   }
 
 }
