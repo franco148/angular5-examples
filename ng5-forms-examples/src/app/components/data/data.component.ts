1a87f59fef730f9366a1236bd1d66ae0aa6813cf
franco-extreme <franck_ral@hotmail.com>
2018-03-14 22:24:32 -0400

    ngAngularForms: Data - Learning about how to reset an Angular Form and loading an Angular Form from a object information.

--- ng5-forms-examples/src/app/components/data/data.component.ts
+++ ng5-forms-examples/src/app/components/data/data.component.ts
@@ -11,7 +11,7 @@ export class DataComponent {
 
   dataForm:FormGroup;
 
-  user:Object = {
+  user:any = {
     fullName: {
       name: "Franco",
       lastName: "Arratia"
@@ -34,6 +34,22 @@ export class DataComponent {
     // });
 
     // If I need to work with a little more complex objects.
+    // Second change: This can be improved.
+    // this.dataForm = new FormGroup({
+    //   'fullName': new FormGroup({
+    //     'name': new FormControl(this.user.fullName.name, [
+    //                   Validators.required,
+    //                   Validators.minLength(3)
+    //                 ]), //second parameter for validation rules, third async validation
+    //     'lastName': new FormControl(this.user.fullName.lastName, Validators.required)
+    //   }),
+    //   'email': new FormControl(this.user.email, [
+    //                 Validators.required,
+    //                 Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')
+    //               ])
+    // });
+
+    //Improving previous approach. The following works if the object has the same structure
     this.dataForm = new FormGroup({
       'fullName': new FormGroup({
         'name': new FormControl('', [
@@ -47,11 +63,21 @@ export class DataComponent {
                     Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')
                   ])
     });
+
+    this.dataForm.setValue(this.user);
+
   }
 
   saveChanges() {
     console.log(this.dataForm.value);
     console.log(this.dataForm);
+
+    //The following code is for cleaning our form, and it is going to have the initial statuses of its properties.
+    //this.dataForm.reset({adding object's properties}); or the following
+    this.dataForm.reset(this.user);
+
+    //Another approach would be, but it is not good.
+    //this.dataForm.controls['email'].setValue('something');
   }
 
 }
