7116cc3c1411ed0bc49fe9b9f9ea0b4e98790595
franco-extreme <franck_ral@hotmail.com>
2018-08-19 20:22:39 -0400

    ng6-material-proj01: Implemented route protection to the application - CanActivate

---
+++ ng6-material-proj01/src/app/auth/auth.guard.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+
+    constructor(private authService: AuthService, private router: Router) {}
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        console.log('Is authenticated: ', this.authService.isAuth());
+        if (this.authService.isAuth()) {
+            return true;
+        }
+        console.log('redirecting to login...');
+        this.router.navigate(['/login']);
+    }
+}
