164a041d8ee06cc312d1ecf904d560061f0d660b
AVANTICA\franco.arratia <franck_ral@hotmail.com>
2018-11-19 15:49:10 -0400

    ng7-virtual-scroll: Implemented Drag&Drop of a ordered list.

--- ng7-virtual-scroll/src/app/app.module.ts
+++ ng7-virtual-scroll/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { ScrollingModule } from '@angular/cdk/scrolling';
 import { DragDropModule } from '@angular/cdk/drag-drop';
@@ -8,17 +10,20 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
 import { AppComponent } from './app.component';
 import { VirtualComponent } from './virtual/virtual.component';
 import { DragComponent } from './drag/drag.component';
+import { CountriesComponent } from './countries/countries.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     VirtualComponent,
-    DragComponent
+    DragComponent,
+    CountriesComponent
   ],
   imports: [
     BrowserModule,
     ScrollingModule,
-    DragDropModule
+    DragDropModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
