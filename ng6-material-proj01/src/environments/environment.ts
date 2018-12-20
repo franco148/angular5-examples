faf75fea2a59651726ca811ea547a4a5208b5d76
AVANTICA\franco.arratia <franck_ral@hotmail.com>
2018-12-20 12:51:28 -0400

    Ng6Material-NgRx: Fixing some issues regarding to info.

--- ng6-material-proj01/src/environments/environment.ts
+++ ng6-material-proj01/src/environments/environment.ts
@@ -5,12 +5,6 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyCO-cI8HTFPC33Gupa1vvfMiDZ0ghhlSEc',
-    authDomain: 'ng-fitness-tracker-fb6fc.firebaseapp.com',
-    databaseURL: 'https://ng-fitness-tracker-fb6fc.firebaseio.com',
-    projectId: 'ng-fitness-tracker-fb6fc',
-    storageBucket: 'ng-fitness-tracker-fb6fc.appspot.com',
-    messagingSenderId: '445535868166'
   }
 };
 
