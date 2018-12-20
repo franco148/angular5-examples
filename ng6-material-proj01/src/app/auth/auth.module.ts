80b24783af2ec519f3b171c1aec0c644eba11d00
franco-extreme <franck_ral@hotmail.com>
2018-12-19 23:39:12 -0400

    Ng6Material-AppOptimization: Splitting up routes - Eager Loading ()

--- ng6-material-proj01/src/app/auth/auth.module.ts
+++ ng6-material-proj01/src/app/auth/auth.module.ts
@@ -5,13 +5,15 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { SharedModule } from '../shared/shared.module';
+import { AuthRoutingModule } from './auth-routing.module';
 
 @NgModule({
   declarations: [LoginComponent, SignupComponent],
   imports: [
     ReactiveFormsModule,
     AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
-    SharedModule
+    SharedModule,
+    AuthRoutingModule
   ],
   exports: []
 })
