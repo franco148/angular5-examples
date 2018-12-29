e2a066e29be8070f444a459032f0510182f1b1a4
franco-extreme <franck_ral@hotmail.com>
2018-12-28 23:31:42 -0400

    Ng6Material-NgRx: Working with multiple reducers & actions.

--- ng6-material-proj01/src/app/app.reducer.ts
+++ ng6-material-proj01/src/app/app.reducer.ts
@@ -1,29 +1,46 @@
 
+// export interface State {
+//     isLoading: boolean;
+// }
+
+// export enum LoadingStates {
+//   START = 'START_LOADING',
+//   STOP = 'STOP_LOADING'
+// }
+
+// const initialState: State = {
+//     isLoading: false
+// };
+
+// export function appReducer(state = initialState, action) {
+//     // Actions must have an action property
+//     switch (action.type) {
+//         case LoadingStates.START:
+//             return {
+//                 isLoading: true
+//             };
+//         case LoadingStates.STOP:
+//             return {
+//                 isLoading: false
+//             };
+//         default:
+//             return state;
+//     }
+// }
+
+// Now here, we are going to have the definition of many reducers.
+
+import * as fromUi from './shared/ui.reducer';
+import { ActionReducerMap, createFeatureSelector, createSelector } from '@ngrx/store';
+
 export interface State {
-    isLoading: boolean;
+  ui: fromUi.State;
 }
 
-export enum LoadingStates {
-  START = 'START_LOADING',
-  STOP = 'STOP_LOADING'
-}
-
-const initialState: State = {
-    isLoading: false
+export const reducers: ActionReducerMap<State> = {
+  ui: fromUi.uiReducer
 };
 
-export function appReducer(state = initialState, action) {
-    // Actions must have an action property
-    switch (action.type) {
-        case LoadingStates.START:
-            return {
-                isLoading: true
-            };
-        case LoadingStates.STOP:
-            return {
-                isLoading: false
-            };
-        default:
-            return state;
-    }
-}
+// Creating feature selectors
+export const getUiState = createFeatureSelector<fromUi.State>('ui');
+export const getIsLoading = createSelector(getUiState, fromUi.getIsLoading);
