06b0f49d5a0a51b5c5c4d3b1e5c49cc8947f1314
franco-extreme <franck_ral@hotmail.com>
2019-01-12 17:02:54 -0400

    Ng6Material-NgRx: Lazy Loaded State

--- ng6-material-proj01/src/app/training/training.reducer.ts
+++ ng6-material-proj01/src/app/training/training.reducer.ts
@@ -1,7 +1,7 @@
 import { TrainingActions, SET_AVAILABLE_TRAININGS, SET_FINISHED_TRAININGS, START_TRAINING, STOP_TRAINING } from './training.actions';
 import { Exercise } from './exercise.model';
 import * as fromRoot from '../app.reducer';
-import { st } from '@angular/core/src/render3';
+import { createFeatureSelector, createSelector } from '@ngrx/store';
 
 
 export interface TrainingState {
@@ -20,7 +20,7 @@ const initialState: TrainingState = {
   activeTraining: null
 };
 
-export function authReducer(state = initialState, action: TrainingActions) {
+export function trainingReducer(state = initialState, action: TrainingActions) {
 
   switch (action.type) {
     case SET_AVAILABLE_TRAININGS:
@@ -53,6 +53,8 @@ export function authReducer(state = initialState, action: TrainingActions) {
   }
 }
 
-export const getAvailableExercises = (state: TrainingState) => state.availableExercises;
-export const getFinishedExercises = (state: TrainingState) => state.finishedExercises;
-export const getActiveTraining = (state: TrainingState) => state.activeTraining;
+export const getTrainingState = createFeatureSelector<TrainingState>('training');
+
+export const getAvailableExercises = createSelector(getTrainingState, (state: TrainingState) => state.availableExercises);
+export const getFinishedExercises = createSelector(getTrainingState, (state: TrainingState) => state.finishedExercises);
+export const getActiveTraining = createSelector(getTrainingState, (state: TrainingState) => state.activeTraining);
