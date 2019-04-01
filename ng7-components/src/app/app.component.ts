847da55c1d9bfcc180ca961cfced9557feef1c8a
Franco Arratia <farratia@aptos.com>
2019-04-01 19:15:38 -0400

    ng7-components: Binding to Custom Properties

--- ng7-components/src/app/app.component.ts
+++ ng7-components/src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   
-  serverElements = [];
+  serverElements = [{type: 'server', name: 'Test Server', content: 'Just a test!'}];
 }
