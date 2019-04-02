a72539e1dd39544e8cfbd39d08b7a3db6adc0f1c
Franco Arratia <farratia@aptos.com>
2019-04-02 09:47:51 -0400

    ng7-components: Assigning an alias to Custom Properties

--- ng7-components/src/app/server-element/server-element.component.ts
+++ ng7-components/src/app/server-element/server-element.component.ts
@@ -7,7 +7,8 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ServerElementComponent implements OnInit {
 
-  @Input() element: { type: string, name: string, content: string };
+  // If input does not have a parameter, it will take the same name of the variable.
+  @Input('srvElement') element: { type: string, name: string, content: string };
 
   constructor() { }
 
