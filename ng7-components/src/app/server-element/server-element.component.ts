4ef1f8291ceb712d7ad324f5523fa53517839434
Franco Arratia <franck_ral@hotmail.com>
2019-04-24 17:54:07 -0400

    ng7-components: View Encapsulation.

--- ng7-components/src/app/server-element/server-element.component.ts
+++ ng7-components/src/app/server-element/server-element.component.ts
@@ -1,9 +1,14 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
+  // , encapsulation: ViewEncapsulation.None ,
+  // - Emulated (default one), 
+  // - Native: This uses the shadow dom technology
+  // - None: With this, all pages (globally) are going to take effect of the style.
+  // - ShadowDom: This will replace to Native later in newer versions.
 })
 export class ServerElementComponent implements OnInit {
 
