4141ba2fd5345d2f13760fe05ad4510a67def0ee
Franco Arratia <franck_ral@hotmail.com>
2019-04-24 17:35:09 -0400

    ng7-components: Assigning an alias to custom events.

--- ng7-components/src/app/cockpit/cockpit.component.ts
+++ ng7-components/src/app/cockpit/cockpit.component.ts
@@ -9,6 +9,7 @@ export class CockpitComponent implements OnInit {
 
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   @Output() bluePrintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  // @Output("bpCreated") bluePrintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
 
   newServerName = '';
   newServerContent = '';
