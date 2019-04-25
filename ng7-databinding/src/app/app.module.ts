eb200b7d17936bd2d0c6edab1e0a2850ff94d368
Franco Arratia <franck_ral@hotmail.com>
2019-04-25 19:08:11 -0400

    ng7-components: Databinding assignment implementation.

--- ng7-databinding/src/app/app.module.ts
+++ ng7-databinding/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EvenComponent } from './even/even.component';
+import { OddComponent } from './odd/odd.component';
+import { GameCtrlComponent } from './game-ctrl/game-ctrl.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EvenComponent,
+    OddComponent,
+    GameCtrlComponent
   ],
   imports: [
     BrowserModule
