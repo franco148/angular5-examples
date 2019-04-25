a43bf3521dda4f656d36b832dc9a55eb46ba449f
Franco Arratia <franck_ral@hotmail.com>
2019-04-25 17:58:40 -0400

    ng7-components: Seeing lifecycle hooks in action.

--- ng7-components/src/app/server-element/server-element.component.ts
+++ ng7-components/src/app/server-element/server-element.component.ts
@@ -1,4 +1,17 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { 
+  Component, 
+  OnInit, 
+  Input, 
+  ViewEncapsulation, 
+  OnChanges, 
+  SimpleChanges, 
+  DoCheck, 
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -10,14 +23,56 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
   // - None: With this, all pages (globally) are going to take effect of the style.
   // - ShadowDom: This will replace to Native later in newer versions.
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit,
+                                               OnChanges,
+                                               DoCheck, 
+                                               AfterContentInit, 
+                                               AfterContentChecked, 
+                                               AfterViewInit, 
+                                               AfterViewChecked,
+                                               OnDestroy {
 
   // If input does not have a parameter, it will take the same name of the variable.
   @Input('srvElement') element: { type: string, name: string, content: string };
+  @Input() name: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() { 
+    console.log('Constructor called!');
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges called!');
+    console.log(changes);
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit called!');
+  }
+
+  ngDoCheck(): void {
+    // Called whenever Angular checks for any changes
+    // and there are a couple of triggers.
+    console.log('ngDoCheck called!');
+  }
+
+  ngAfterContentInit(): void {
+    //Called after ngOnInit when the component's or directive's content has been initialized.
+    console.log('ngAfterContentInit called!');
+  }
+
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked called!');
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit called!');
+  }
+
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked called!');
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy called!');
+  }
 }
