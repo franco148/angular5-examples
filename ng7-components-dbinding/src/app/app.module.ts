43e87cdafcd13f63df39abe48cc708555c1f0b5e
Franco Arratia <franck_ral@hotmail.com>
2019-04-29 19:21:17 -0400

    ng7-ComponentsAndDatabinding: Creating Header, Recipes, sub items in recipes

--- ng7-components-dbinding/src/app/app.module.ts
+++ ng7-components-dbinding/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipeDetailComponent,
+    RecipeListComponent,
+    RecipesComponent,
+    RecipeItemComponent
   ],
   imports: [
     BrowserModule
