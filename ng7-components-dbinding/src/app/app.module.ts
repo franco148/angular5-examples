d7c5f913a8df4a68baf977d365eda8d775ea02de
Franco Arratia <franck_ral@hotmail.com>
2019-04-30 10:36:56 -0400

    ng7-ComponentsAndDatabinding: Added new components ShoppingList, ShoppingEdit, and a Ingredient Model

--- ng7-components-dbinding/src/app/app.module.ts
+++ ng7-components-dbinding/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,9 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
     RecipeDetailComponent,
     RecipeListComponent,
     RecipesComponent,
-    RecipeItemComponent
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule
