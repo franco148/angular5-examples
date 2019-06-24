fdc09266f6fb4755440ee85b74260594f88b7c3d
Franco Arratia <franck_ral@hotmail.com>
2019-06-24 18:59:50 -0400

    ng7-DirectivesDeepDive: Created a new custom directive.

--- ng7-directives-deep/src/app/app.module.ts
+++ ng7-directives-deep/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BasicHighlightComponent } from './basic-highlight/basic-highlight.component';
 import { BetterHighlightComponent } from './better-highlight/better-highlight.component';
+import { UnlessDirective } from './unless.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     BasicHighlightComponent,
-    BetterHighlightComponent
+    BetterHighlightComponent,
+    UnlessDirective
   ],
   imports: [
     BrowserModule,
