20336cefe85abec7d44870c0ecf16a78634ed15d
Franco Arratia <franck_ral@hotmail.com>
2019-06-24 18:23:31 -0400

    ng7-ComponentsAndDatabinding: Allowing the User to Add Ingredients to the Shopping List.

--- ng7-components-dbinding/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ ng7-components-dbinding/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, EventEmitter, Output } from '@angular/core';
+import { Ingredient } from 'app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  // The following with Angular 8 should be: @ViewChild('...', {static:false})
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountInput') amountInputRef: ElementRef;
+
+  // ingredientAdded = new EventEmitter<{name: string, amount: number}>();
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onAddItem() {
+    const ingredientName = this.nameInputRef.nativeElement.value;
+    const ingredientAmount = this.amountInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(ingredientName, ingredientAmount);
+
+    this.ingredientAdded.emit(newIngredient);
+  }
 }
