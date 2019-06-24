85e5a47fda710ce791ce231b73cabd07d0190212
Franco Arratia <franck_ral@hotmail.com>
2019-06-24 19:04:57 -0400

    ng7-DirectivesDeepDive: Added the implementation of  basic-highlight directive.

--- ng7-directives-deep/src/app/basic-highlight/basic-highlight.directive.ts
+++ ng7-directives-deep/src/app/basic-highlight/basic-highlight.directive.ts
@@ -1,10 +1,15 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appBasicHighlight]'
 })
 export class BasicHighlightDirective {
 
-  constructor() { }
+  constructor(private elementRef: ElementRef) {
+  }
+
+  ngOnInit() {
+    this.elementRef.nativeElement.style.backgroundColor = 'green';
+  }
 
 }
