b752eeaeea04431ab3d0f32ffb7de0d0b5e87b8a
Franco Arratia <franck_ral@hotmail.com>
2019-06-26 16:31:53 -0400

    ng7-services-dependencyInjection: Implemented account component.

--- ng7-services-di/src/app/account/account.component.ts
+++ ng7-services-di/src/app/account/account.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css']
 })
-export class AccountComponent implements OnInit {
+export class AccountComponent {
 
-  constructor() { }
+  @Input() account: {name: string, status: string};
+  @Input() id: number;
+  @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
 
-  ngOnInit() {
+
+  onSetTo(status: string) {
+    this.statusChanged.emit({id: this.id, newStatus: status});
+    console.log('A server status changed, new status: ' + status);
   }
 
 }
