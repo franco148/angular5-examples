c27e64c1757cf583615a2c1979774d4e3214481a
Franco Arratia <franck_ral@hotmail.com>
2019-07-01 17:50:37 -0400

    ng7-routing: Implemented USERS components.

--- ng7-routing/src/app/users/user/user.component.ts
+++ ng7-routing/src/app/users/user/user.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
+  user: {id: number, name: string};
+
   constructor() { }
 
   ngOnInit() {
-  }
+  } 
 
 }
