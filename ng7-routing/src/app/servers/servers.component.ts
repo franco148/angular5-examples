daaf80c3da6f81ead954169e1feab20301e42182
Franco Arratia <franck_ral@hotmail.com>
2019-07-01 17:48:22 -0400

    ng7-routing: Implemented main SERVERS component.

--- ng7-routing/src/app/servers/servers.component.ts
+++ ng7-routing/src/app/servers/servers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ServersService } from './servers.service';
 
 @Component({
   selector: 'app-servers',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  private servers: {id: number, name: string, status: string}[] = [];
+
+  constructor(private serversService: ServersService) { }
 
   ngOnInit() {
-  }
+    this.servers = this.serversService.getServers();
+  } 
 
 }
