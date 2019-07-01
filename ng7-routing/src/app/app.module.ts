2e4326ae8fd06a2fd66496148561aa0c6f6d2628
Franco Arratia <franck_ral@hotmail.com>
2019-07-01 17:39:19 -0400

    ng7-routing: Added the SERVERS components.

--- ng7-routing/src/app/app.module.ts
+++ ng7-routing/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { ServersComponent } from './servers/servers.component';
+import { ServerComponent } from './servers/server/server.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ServersComponent,
+    ServerComponent,
+    EditServerComponent
   ],
   imports: [
     BrowserModule,
