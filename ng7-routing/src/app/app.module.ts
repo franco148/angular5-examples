ce04cf5278fa7f24ebe01245756ee4e92294f89a
Franco Arratia <franck_ral@hotmail.com>
2019-07-01 17:40:20 -0400

    ng7-routing: Added the USERS components.

--- ng7-routing/src/app/app.module.ts
+++ ng7-routing/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HomeComponent } from './home/home.component';
 import { ServersComponent } from './servers/servers.component';
 import { ServerComponent } from './servers/server/server.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { UsersComponent } from './users/users.component';
+import { UserComponent } from './users/user/user.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
     HomeComponent,
     ServersComponent,
     ServerComponent,
-    EditServerComponent
+    EditServerComponent,
+    UsersComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
