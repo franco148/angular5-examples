f97ed69d483d5d9b61172d66473334a1e6f0aa28
Franco Arratia <franck_ral@hotmail.com>
2019-07-02 20:44:02 -0400

    ng7-routing: Implementing the routing module.

--- ng7-routing/src/app/app-routing.module.ts
+++ ng7-routing/src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { UserComponent } from './users/user/user.component';
+import { ServersComponent } from './servers/servers.component';
+import { ServerComponent } from './servers/server/server.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'users', component: UsersComponent, children: [
+    { path: ':id/:name', component: UserComponent }
+  ] },
+  {
+    path: 'servers',
+    // canActivate: [AuthGuard],
+    // canActivateChild: [AuthGuard],
+    component: ServersComponent,
+    children: [
+    // { path: ':id', component: ServerComponent, resolve: {server: ServerResolver} },
+    // { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard] }
+  ] },
+  // { path: 'not-found', component: PageNotFoundComponent },
+  // { path: 'not-found', component: ErrorPageComponent, data: {message: 'Page not found!'} },
+  { path: '**', redirectTo: '/not-found' }
+]; 
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
