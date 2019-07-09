205cb9000e9bfeeb7f75d90a52c155c03396ea41
Franco Arratia <franck_ral@hotmail.com>
2019-07-09 19:21:56 -0400

    ng7-routing: Fixed issues regarding to Forms

--- ng7-routing/src/app/app.module.ts
+++ ng7-routing/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -31,6 +33,7 @@ import { AuthGuard } from './auth-guard.service';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [AuthService, ServersService, CanDeactivateGuard, ServerResolver, AuthGuard],
