eed5475663ba86f2ec884aadf148051a20eb114a
Franco Arratia <franck_ral@hotmail.com>
2019-07-11 11:16:31 -0400

    ng7-observables-start: Updated routing and app modules.

--- ng7-observables-start/src/app/app-routing.module.ts
+++ ng7-observables-start/src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-// import { HomeComponent } from './home/home.component';
-// import { UserComponent } from './user/user.component';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
 
 const routes: Routes = [
-//   {path: '', component: HomeComponent},
-//   {path: 'user/:id', component: UserComponent}
+  {path: '', component: HomeComponent},
+  {path: 'user/:id', component: UserComponent}
 ];
 
 @NgModule({
