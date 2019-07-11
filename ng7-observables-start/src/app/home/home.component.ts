40bdfa18e31c89640d6ead333d0f8ba9593a863e
Franco Arratia <franck_ral@hotmail.com>
2019-07-11 12:12:14 -0400

    ng7-observables-start: Understanding operators.

--- ng7-observables-start/src/app/home/home.component.ts
+++ ng7-observables-start/src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription, interval, Observable } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-home',
@@ -35,14 +37,27 @@ export class HomeComponent implements OnInit, OnDestroy {
       }, 1000);
     });
 
-    this.firstObsSubscription = customIntervalObservable.subscribe(data => {
+    // this.firstObsSubscription = customIntervalObservable.subscribe(data => {
+    //   console.log(data);
+    // }, error => {
+    //   console.log(error);
+    //   alert(error.message);
+    // }, () => {
+    //   console.log('Completed!');
+    // });
+
+    this.firstObsSubscription = customIntervalObservable.pipe(filter(data => {
+      return data > 0;
+    }), map((data: number) => {
+      return 'Round: ' + (data + 1);
+    })).subscribe(data => {
       console.log(data);
     }, error => {
       console.log(error);
       alert(error.message);
     }, () => {
       console.log('Completed!');
-    });
+    }); 
   }
 
   ngOnDestroy(): void {
