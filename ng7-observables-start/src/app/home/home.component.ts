6256cda2a8ebf167569e8506aec692859432994f
Franco Arratia <franck_ral@hotmail.com>
2019-07-11 12:03:12 -0400

    ng7-observables-start: Building a Custom Observable.

--- ng7-observables-start/src/app/home/home.component.ts
+++ ng7-observables-start/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Subscription, interval } from 'rxjs';
+import { Subscription, interval, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -17,9 +17,32 @@ export class HomeComponent implements OnInit, OnDestroy {
     //   console.log(count);
     // });
 
-    this.firstObsSubscription = interval(1000).subscribe(count => {
-      console.log(count);
-    }); 
+    // this.firstObsSubscription = interval(1000).subscribe(count => {
+    //   console.log(count);
+    // }); 
+
+    const customIntervalObservable = Observable.create(observer => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        if (count === 5) {
+          observer.complete();
+        }
+        if (count > 3) {
+          observer.error(new Error('Count is greater 3!'));
+        }
+        count++;
+      }, 1000);
+    });
+
+    this.firstObsSubscription = customIntervalObservable.subscribe(data => {
+      console.log(data);
+    }, error => {
+      console.log(error);
+      alert(error.message);
+    }, () => {
+      console.log('Completed!');
+    });
   }
 
   ngOnDestroy(): void {
