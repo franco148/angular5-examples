b01546bb9d829c5985d4d4beef31140dc868e26b
Franco Arratia <franck_ral@hotmail.com>
2019-07-11 11:13:53 -0400

    ng7-observables-start: Implemented USER component.

--- ng7-observables-start/src/app/app.module.ts
+++ ng7-observables-start/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule
