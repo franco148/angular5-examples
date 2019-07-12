56cb7bc20b7502251f6a23e5ed92be54b91c1b1c
Franco Arratia <franck_ral@hotmail.com>
2019-07-12 18:36:36 -0400

    ng7-project-observables: Updated Recipe Service and Recipes component with observables.

--- ng7-components-dbinding/src/app/recipes/recipes.component.ts
+++ ng7-components-dbinding/src/app/recipes/recipes.component.ts
@@ -9,17 +9,24 @@ import { RecipeService } from './recipe.service';
   providers: [RecipeService] 
 })
 export class RecipesComponent implements OnInit {
-  selectedRecipe: Recipe;
+  // selectedRecipe: Recipe;
 
-  constructor(private recipeService: RecipeService) { }
+  // constructor(private recipeService: RecipeService) { }
+
+  // ngOnInit() {
+  //   this.recipeService.recipeSelected
+  //     .subscribe(
+  //       (recipe: Recipe) => {
+  //         this.selectedRecipe = recipe;
+  //       }
+  //     ); 
+  // }
+
+  constructor() {
+  }
 
   ngOnInit() {
-    this.recipeService.recipeSelected
-      .subscribe(
-        (recipe: Recipe) => {
-          this.selectedRecipe = recipe;
-        }
-      ); 
-  }
+
+  } 
 
 }
