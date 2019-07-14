095603f07004b348dd003612111f6b2379b339c8
franco-extreme <franck_ral@hotmail.com>
2019-07-13 22:36:46 -0400

    ng8-forms-templatedriven: Submitting and using the Form.

--- ng8-forms-td/src/app/app.component.ts
+++ ng8-forms-td/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,14 @@ import { Component } from '@angular/core';
 export class AppComponent {
   suggestUserName() {
     const suggestedName = 'Superuser';
+  }
+
+  // Whe in the html we use #alias
+  // onSubmit(form: ElementRef) {
+  //   console.log(form);
+  // }
+
+  onSubmit(form: NgForm) {
+    console.log(form);
   }
 }
