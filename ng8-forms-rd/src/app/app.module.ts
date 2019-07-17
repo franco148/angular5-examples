6e827298ce796795bfeaa6a8f5ae1c4f75423e75
franco-extreme <franck_ral@hotmail.com>
2019-07-16 22:19:06 -0400

    ng8-forms-reactivedriven: Reactive setup

--- ng8-forms-rd/src/app/app.module.ts
+++ ng8-forms-rd/src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +11,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
