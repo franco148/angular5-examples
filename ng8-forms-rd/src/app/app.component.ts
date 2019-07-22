90dd5416813706ac15fe90348f7fa3460bb87eae
Franco Arratia <franck_ral@hotmail.com>
2019-07-22 12:43:41 -0400

    ng8-forms-reactivedriven: Creating custom validators.

--- ng8-forms-rd/src/app/app.component.ts
+++ ng8-forms-rd/src/app/app.component.ts
@@ -11,6 +11,7 @@ export class AppComponent implements OnInit {
 
   genders = ['male', 'female'];
   signupForm: FormGroup;
+  forbiddenUserNames = ['Chris', 'Anna'];
 
   ngOnInit() {
     // this.signupForm = new FormGroup({
@@ -19,9 +20,12 @@ export class AppComponent implements OnInit {
     //   'gender': new FormControl('male')
     // });
 
+    // Adding our first custom validator we are going to get an error. What is wrong there?
+    // The error is how javascript handles this keyword. That happens because angular is going to
+    // execute the validation, not us. So we actually need to bind THIS.
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
-        'username': new FormControl(null, Validators.required),
+        'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
         'email': new FormControl(null, [Validators.required, Validators.email])
       }),
       'gender': new FormControl('male'),
@@ -38,5 +42,13 @@ export class AppComponent implements OnInit {
 
     // We need to explicitly cast
     (<FormArray>this.signupForm.get('hobbies')).push(control);
+  }
+
+  forbiddenNames(control: FormControl): {[s: string]: boolean} {
+    if (this.forbiddenUserNames.indexOf(control.value) !== -1) {
+      return {'nameIsForbidden': true};
+    }
+
+    return null;
   }
 }
