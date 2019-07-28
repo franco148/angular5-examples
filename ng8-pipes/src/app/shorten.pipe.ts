c8a9621c797bf75845ce7d44b9b002c52fd9280d
franco-extreme <franck_ral@hotmail.com>
2019-07-28 16:30:01 -0400

    ng8-pipes: Creating a custom pipe

---
+++ ng8-pipes/src/app/shorten.pipe.ts
@@ -0,0 +1,15 @@
+import { PipeTransform, Pipe } from '@angular/core';
+
+@Pipe({
+  name: 'shorten'
+})
+export class ShortenPipe implements PipeTransform {
+
+  transform(value: any) {
+    if (value.length) {
+      return value.substr(0, 10) + ' ...';
+    }
+
+    return value;
+  }
+}
