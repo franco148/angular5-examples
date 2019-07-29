436cb2e7052c64d69651c262199b3c407547efa2
Franco Arratia <franck_ral@hotmail.com>
2019-07-29 16:35:42 -0400

    ng8-pipes: Pure and impure pipes.

--- ng8-pipes/src/app/filter.pipe.ts
+++ ng8-pipes/src/app/filter.pipe.ts
@@ -1,7 +1,18 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Button } from 'protractor';
+import { attachEmbeddedView } from '@angular/core/src/view';
+import { timeout } from 'q';
+import { areAllEquivalent, not } from '@angular/compiler/src/output/output_ast';
+import { AbstractControlDirective } from '@angular/forms';
 
+// Currently we have an 'issue', it happens when you are filtering, then clicking Add Server Button,
+// You will not able to see those changes, however, those new items will be added.
+// It may be a bad idea to filter or recalculate the pipe all the time, but it will depend on the scenario
+// you are.
+// For fixing the issue mentioned above, we need to specify if this is a pure pipe or not.
 @Pipe({
-  name: 'filter'
+  name: 'filter',
+  pure: false
 })
 export class FilterPipe implements PipeTransform {
 
