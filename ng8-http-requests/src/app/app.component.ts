0c98fe379347ccf0dbb1789de00c5a8dad0bd511
Franco Arratia <franck_ral@hotmail.com>
2019-07-31 17:31:54 -0400

    ng8-http-requests: Using types with the HttpClient.

--- ng8-http-requests/src/app/app.component.ts
+++ ng8-http-requests/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { Post } from './post.model';
 
 
 
@@ -22,7 +23,7 @@ export class AppComponent {
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
     this.http
-      .post(
+      .post<{ name: string }>(
         'https://ngheroesfirebase.firebaseio.com/posts.json',
         postData
       )
@@ -41,9 +42,25 @@ export class AppComponent {
   }
 
   private fetchPosts() {
-    this.http.get('https://ngheroesfirebase.firebaseio.com/posts.json')
+    // First approach for using Types with HttpClient
+    // this.http.get('https://ngheroesfirebase.firebaseio.com/posts.json')
+    //          .pipe(map((response: { [key: string]: Post }) => {
+    //            const postsArray: Post[] = [];
+    //            for (const value in response) {
+    //              if (response.hasOwnProperty(value)) {
+    //               postsArray.push({...response[value], id: value});
+    //              }
+    //            }
+    //            return postsArray;
+    //          }))
+    //          .subscribe(posts => {
+    //             console.log(posts);
+    //          });
+
+    // Second approach
+    this.http.get<{ [key: string]: Post }>('https://ngheroesfirebase.firebaseio.com/posts.json')
              .pipe(map(response => {
-               const postsArray = [];
+               const postsArray: Post[] = [];
                for (const value in response) {
                  if (response.hasOwnProperty(value)) {
                   postsArray.push({...response[value], id: value});
