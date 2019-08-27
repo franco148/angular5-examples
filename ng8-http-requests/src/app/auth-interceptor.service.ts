e793ce2493ffccdc376c9e90348d536d4236b095
Franco Arratia <franck_ral@hotmail.com>
2019-08-27 18:55:21 -0400

    ng8-http-requests: Manipulating request objects.

--- ng8-http-requests/src/app/auth-interceptor.service.ts
+++ ng8-http-requests/src/app/auth-interceptor.service.ts
@@ -4,6 +4,14 @@ export class AuthInterceptorService implements HttpInterceptor {
 
     intercept(req: HttpRequest<any>, next: HttpHandler) {
         console.log('Request is on its way');
-        return next.handle(req);
+        // The request object is unmutable, so we can not do the following, it will not work
+        // req.url = 'a new url';
+
+        // So the previous one would be solved as follows
+        // const modifiedRequest = req.clone({url: 'new urld', headers: req.headers.append('', '')});
+        const modifiedRequest = req.clone({headers: req.headers.append('Auth', 'xyz')});
+        
+        // return next.handle(req);
+        return next.handle(modifiedRequest);
     }
 }
