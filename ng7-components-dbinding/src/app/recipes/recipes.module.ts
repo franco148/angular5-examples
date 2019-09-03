c9e57374feb2c889ceb24ea6358b3070c19aea6d
Franco Arratia <franck_ral@hotmail.com>
2019-09-03 16:25:56 -0400

    project-modules-optimization: Splitting modules correctly.

--- ng7-components-dbinding/src/app/recipes/recipes.module.ts
+++ ng7-components-dbinding/src/app/recipes/recipes.module.ts
@@ -1,4 +1,7 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
@@ -15,6 +18,15 @@ import { RecipeStartComponent } from './recipe-start/recipe-start.component';
         RecipeItemComponent,
         RecipeEditComponent,
         RecipeStartComponent
+    ],
+    // The following error is shown: Can't bind to 'ngForOf' since it isn't a known property of 'li'. ("
+    // First it was regarding to router-outlet, now it is because it can not find ngFor, this is
+    // in BrowserModule, however it is not a good practice to have declared this module in many places,
+    // So it can be used CommonModule.
+    imports: [
+        RouterModule,
+        CommonModule,
+        ReactiveFormsModule
     ],
     exports: [
         RecipeDetailComponent,
