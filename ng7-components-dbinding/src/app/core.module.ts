cb690e023814d83277eb0f18b654f6ea5c3e1829
Franco Arratia <franck_ral@hotmail.com>
2019-09-04 11:31:40 -0400

    project-modules-optimization: Understanding the CORE module.

---
+++ ng7-components-dbinding/src/app/core.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { ShoppingListService } from './shopping-list/shopping-list.service';
+import { RecipeService } from './recipes/recipe.service';
+import { AuthInterceptorService } from './auth/auth-interceptor.service';
+
+// Here we do not need to export the services because services work differently than declarations,
+// only declarations and other modules need to be exported, services are automatically injected on
+// a root level, you do not need to export them to make this happen
+@NgModule({
+    providers: [
+        ShoppingListService, 
+        RecipeService,
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: AuthInterceptorService,
+            multi: true
+        }
+    ]
+})
+export class CoreModule {}
