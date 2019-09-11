fed610646f7c1dff6db4144fc9c66d3f9e1bd290
Franco Arratia <franck_ral@hotmail.com>
2019-09-11 17:25:30 -0400

    project-ngrx: Applying NgRx features to RecipeList component.

--- ng7-components-dbinding/src/app/recipes/recipe-list/recipe-list.component.ts
+++ ng7-components-dbinding/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit, EventEmitter, Output, OnDestroy } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+
+import { Store } from '@ngrx/store';
 import { Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
+import * as fromApp from '../../store/app.reducer';
 
 @Component({
   selector: 'app-recipe-list',
@@ -19,18 +23,29 @@ export class RecipeListComponent implements OnInit, OnDestroy {
   recipes: Recipe[];
   subscription: Subscription;
 
-  constructor(private recipeService: RecipeService,
-              private router: Router,
-              private route: ActivatedRoute) {
-  } 
+  // constructor(private recipeService: RecipeService,
+  //             private router: Router,
+  //             private route: ActivatedRoute) {
+  // } 
+
+  constructor(private router: Router,
+              private route: ActivatedRoute,
+              private store: Store<fromApp.AppState>) {}
 
   ngOnInit() {
-    this.subscription = this.recipeService
-                            .recipesChanged
-                            .subscribe((recipes: Recipe[]) => {
-                              this.recipes = recipes;
-                            })    
-    this.recipes = this.recipeService.getRecipes();
+    // this.subscription = this.recipeService
+    //                         .recipesChanged
+    //                         .subscribe((recipes: Recipe[]) => {
+    //                           this.recipes = recipes;
+    //                         })    
+    // this.recipes = this.recipeService.getRecipes();
+
+    this.subscription = this.store
+      .select('recipes')
+      .pipe(map(recipesState => recipesState.recipes))
+      .subscribe((recipes: Recipe[]) => {
+        this.recipes = recipes;
+      }); 
   }
 
   // onRecipeSelected(recipe: Recipe) {
