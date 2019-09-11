5bd272bc2f94ec6e935f37c8a69d3e39080523c2
Franco Arratia <franck_ral@hotmail.com>
2019-09-11 16:40:24 -0400

    project-ngrx: Implemented NgRx features in Recipes module.

--- ng7-components-dbinding/src/app/header/header.component.ts
+++ ng7-components-dbinding/src/app/header/header.component.ts
@@ -1,9 +1,12 @@
 import { Component, Output, EventEmitter, OnInit, OnDestroy } from '@angular/core';
 
+import { Store } from '@ngrx/store';
 import { Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
 
-import { DataStorageService } from 'app/shared/data-storage.service';
-import { AuthService } from 'app/auth/auth.service';
+import * as fromApp from '../store/app.reducer';
+import * as AuthActions from '../auth/store/auth.actions';
+import * as RecipeActions from '../recipes/store/recipe.actions';
 
 @Component({
   selector: 'app-header',
@@ -20,25 +23,35 @@ export class HeaderComponent implements OnInit, OnDestroy {
   //   this.featureSelected.emit(feature);
   // }
 
-  constructor(private dataStorageService: DataStorageService,
-              private authService: AuthService) {}
+  constructor(private store: Store<fromApp.AppState>) {}
 
   ngOnInit() {
-    this.userSubscription = this.authService.user.subscribe(user => {
-      this.isAuthenticated = !!user;
-    });
+    // this.userSubscription = this.authService.user.subscribe(user => {
+    //   this.isAuthenticated = !!user;
+    // });
+    this.userSubscription = this.store
+      .select('auth')
+      .pipe(map(authState => authState.user))
+      .subscribe(user => {
+        this.isAuthenticated = !!user;
+        console.log(!user);
+        console.log(!!user);
+      });
   }
 
   onSaveData() {
-    this.dataStorageService.storeRecipes();
+    // this.dataStorageService.storeRecipes();
+    this.store.dispatch(new RecipeActions.StoreRecipes());
   }
 
   onFetchData() {
-    this.dataStorageService.fetchRecipes().subscribe();
+    // this.dataStorageService.fetchRecipes().subscribe();
+    this.store.dispatch(new RecipeActions.FetchRecipes());
   }
 
   onLogout() {
-    this.authService.logout();
+    // this.authService.logout();
+    this.store.dispatch(new AuthActions.Logout());
   }
 
   ngOnDestroy() {
