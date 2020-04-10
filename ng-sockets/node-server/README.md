4322f1cdca103c49d2bca57868e26007d7ca0f2d
Franco Arratia <franck_ral@hotmail.com>
2020-04-10 16:06:03 -0400

    sockets-angular-nodejs: Cors configuration.

--- ng-sockets/node-server/README.md
+++ ng-sockets/node-server/README.md
@@ -3,9 +3,11 @@
 - npm install cors
 - npm install body-parser
 - npm install @types/express --save-dev # Suggested by node
+- npm install @types/cors --save-dev # Suggested by node
 
 
 ### Extra tools
 For using typescript in node js we would need to configure it. So for that execute `tsc --init`
 Other required commands:
 - tsc -w
+- 
--- ng-sockets/node-server/index.ts
+++ ng-sockets/node-server/index.ts
@@ -1,4 +1,5 @@
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 import Server from "./classes/server";
 import { SERVER_PORT } from "./global/environment";
@@ -9,6 +10,10 @@ const server = new Server();
 server.app.use(bodyParser.urlencoded({extended: true}));
 server.app.use(bodyParser.json());
 
+// CORS
+server.app.use(cors({ origin: true, credentials: true }))
+
+// Endpoints Definition
 server.app.use('/', router);
 
 server.start(() => {
