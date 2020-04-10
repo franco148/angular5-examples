8ce19da56d1e3ee6534c096137bdd1b7b20c052f
Franco Arratia <franck_ral@hotmail.com>
2020-04-10 15:26:20 -0400

    reactjs-client-development: Creating the first endpoints.

--- ng-sockets/node-server/index.ts
+++ ng-sockets/node-server/index.ts
@@ -1,7 +1,9 @@
 import Server from "./classes/server";
 import { SERVER_PORT } from "./global/environment";
+import router from "./routes/router";
 
 const server = new Server();
+server.app.use('/', router);
 
 server.start(() => {
     console.log(`The web socket server is running at port: ${SERVER_PORT}`);
