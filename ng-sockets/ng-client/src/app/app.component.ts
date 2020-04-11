7d147bddab3d6eb15c3df74ffea5986b95f68c51
Franco Arratia <franck_ral@hotmail.com>
2020-04-11 08:28:45 -0400

    sockets-angular-nodejs: Verifying server state from the client.

--- ng-sockets/ng-client/src/app/app.component.ts
+++ ng-sockets/ng-client/src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WebsocketService } from './services/websocket.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ng-client';
+export class AppComponent implements OnInit {
+  
+  constructor(wsService: WebsocketService) {}
+
+  ngOnInit() {}
 }
---
+++ ng-sockets/ng-client/src/app/services/websocket.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Socket } from 'ngx-socket-io';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WebsocketService {
+
+  socketStatus: boolean = false;
+
+  constructor(private socket: Socket) { 
+    this.checkStatus();
+  }
+
+  checkStatus() {
+    this.socket.on('connect', () => {
+      console.log('Connected to the server...');
+      this.socketStatus = true;
+    });
+
+    this.socket.on('disconnect', () => {
+      console.log('Disconnected to the server...');
+      this.socketStatus = false;
+    });
+  }
+}
