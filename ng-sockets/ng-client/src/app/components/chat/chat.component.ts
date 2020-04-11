1a2173abfc149e7b94cb0a68a572a5fdad4a5759
Franco Arratia <franck_ral@hotmail.com>
2020-04-11 17:19:32 -0400

    sockets-angular-nodejs: Implementing the UI for displaying the new messages.

--- ng-sockets/ng-client/src/app/components/chat/chat.component.ts
+++ ng-sockets/ng-client/src/app/components/chat/chat.component.ts
@@ -11,16 +11,30 @@ export class ChatComponent implements OnInit, OnDestroy {
 
   text = '';
   messageSubscription: Subscription;
+  element: HTMLElement;
+
+  messages: any[] = [];
 
   constructor(private chatService: ChatService) { }
 
   ngOnInit() {
+    this.element = document.getElementById('chat-messages');
+
     this.messageSubscription = this.chatService.getMessage().subscribe(msg => {
       console.log(msg, ' from server!!!');
+      this.messages.push(msg);
+      setTimeout(() => {
+        this.element.scrollTop = this.element.scrollHeight;
+      }, 50);
     });
   }
 
   send() {
+
+    if (this.text.trim().length === 0) {
+      return;
+    }
+
     console.log(this.text);
     this.chatService.sendMessage(this.text);
     this.text = '';
