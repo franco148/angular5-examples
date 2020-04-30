7154c1201a77c1d749a4733148ac054f249dcfa6
Franco Arratia <franck_ral@hotmail.com>
2020-04-30 12:52:04 -0400

    sockets-angular-nodejs: Section7 - Getting started with project with graphs in real time

--- ng-sockets/ng-graphics/src/app/app.module.ts
+++ ng-sockets/ng-graphics/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+const config: SocketIoConfig = { url: 'http://localhost:5000', options: {} };
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SocketIoModule.forRoot(config)
   ],
   providers: [],
   bootstrap: [AppComponent]
