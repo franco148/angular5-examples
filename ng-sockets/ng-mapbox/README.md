5070bfa549932cd240d7dadd0ef4c9a047af5b03
Franco Arratia <franck_ral@hotmail.com>
2020-05-12 18:29:49 -0400

    sockets-angular-nodejs: Show mapbox map initial steps

--- ng-sockets/ng-mapbox/README.md
+++ ng-sockets/ng-mapbox/README.md
@@ -25,3 +25,29 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+
+## Dev Notes
+- Since mapbox-gl is not created for typescript, we will need to install like a intellicense for that. Search it as `mapbox-gl types`
+
+```bash
+npm install --save-dev @types/mapbox-gl
+```
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- ng-sockets/ng-mapbox/src/app/components/boxmap/boxmap.component.ts
+++ ng-sockets/ng-mapbox/src/app/components/boxmap/boxmap.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as mapboxgl from "mapbox-gl";
+
 @Component({
   selector: 'app-boxmap',
   templateUrl: './boxmap.component.html',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoxmapComponent implements OnInit {
 
+  map: mapboxgl.Map;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.createMap();
   }
 
+  createMap() {
+    (mapboxgl as any).accessToken = 'pk.eyJ1IjoiZnJhbmNvZnJhbCIsImEiOiJja2EzOTkydTQwbTkxM25xa3J6bThmb2hmIn0.UghzC-hUNQPAjKOt794qhA';
+    this.map = new mapboxgl.Map({
+      container: 'boxmap',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-75.75512993582937, 45.349977429009954],
+      zoom: 15.8
+    });
+  }
 }
