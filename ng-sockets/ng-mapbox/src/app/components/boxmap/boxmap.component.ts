211ef6e272b6c350c6c903d76f3a8167560fc91c
Franco Arratia <franck_ral@hotmail.com>
2020-05-11 22:01:06 -0400

    sockets-angular-nodejs: Configure the new MapBox project

---
+++ ng-sockets/ng-mapbox/src/app/components/boxmap/boxmap.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-boxmap',
+  templateUrl: './boxmap.component.html',
+  styleUrls: ['./boxmap.component.css']
+})
+export class BoxmapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
