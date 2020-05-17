5d66dd56ba5206dee1aeeeef43d141f7f90a1746
Franco Arratia <franck_ral@hotmail.com>
2020-05-17 17:40:46 -0400

    sockets-angular-nodejs: Section 9 - Connectiong angular application to the server via websockets.

--- ng-sockets/ng-mapbox/src/app/app.module.ts
+++ ng-sockets/ng-mapbox/src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { SocketIoModule, SocketIoConfig } from "ngx-socket-io";
+
 import { AppComponent } from './app.component';
 import { BoxmapComponent } from './components/boxmap/boxmap.component';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,8 @@ import { BoxmapComponent } from './components/boxmap/boxmap.component';
     BoxmapComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SocketIoModule.forRoot(environment.socketConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
