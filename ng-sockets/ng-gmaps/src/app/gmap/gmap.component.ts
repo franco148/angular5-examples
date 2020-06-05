1bd29cd001fb722df74fe9182520330553a0398f
Franco Arratia <franck_ral@hotmail.com>
2020-06-04 20:46:43 -0400

    sockets-angular-nodejs: Showing the map, this first version does not include the complete project with google maps

--- ng-sockets/ng-gmaps/src/app/gmap/gmap.component.ts
+++ ng-sockets/ng-gmaps/src/app/gmap/gmap.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-gmap',
   templateUrl: './gmap.component.html',
   styleUrls: ['./gmap.component.css']
 })
-export class GmapComponent implements OnInit {
+export class GmapComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('gmap') gmapElement: ElementRef;
+  gmap: google.maps.Map;
 
   constructor() { }
 
   ngOnInit(): void {
+    // this.loadGMap();
+  }
+
+  loadGMap() {
+    const latLng = new google.maps.LatLng(37.784679, -122.395936);
+
+    const optionsGMap: google.maps.MapOptions = {
+      center: latLng,
+      zoom: 13,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    // console.log(this.gmapElement);
+    this.gmap = new google.maps.Map(this.gmapElement.nativeElement, optionsGMap);
+  }
+
+  ngAfterViewInit() {
+    console.log('AAA   ', this.gmapElement.nativeElement);
+    this.loadGMap();
   }
 
 }
